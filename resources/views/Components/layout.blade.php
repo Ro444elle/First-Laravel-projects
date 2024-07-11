254be85d37d9252b21ee614eccd331f78c63f07e
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2024-07-11 10:12:46 +0300

    up

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+</head>
+
+<body>
+  <nav>
+    <a href="/">HOME</a>
+    <a href="/about">About Page</a>
+    <a href="/contact">Contact Page</a>
+  </nav>
+
+  <h1>Hello from HOME page</h1>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Page</title>
+</head>
+<body>
+  <h1>Here you can contact us</h1>
+  <p>Our email adress is: contact@contact.com</p>
+</body>
+</html>
