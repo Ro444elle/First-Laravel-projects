802e468a1cad13104823a526273b3ad401235320
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2024-07-15 09:13:33 +0300

    second commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Page</title>
-</head>
-<body>
-  <h1>Here you can contact us</h1>
-  <p>Our email adress is: contact@contact.com</p>
-</body>
-</html>
+<x-layout>
+
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <h1>Welcome to Contact PAGE</h1>
+</x-layout>
